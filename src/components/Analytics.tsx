2a9325814be04bf32d63bff3acd554a2a9830075
Yunlong Li <yunlong.li@csiro.au>
2024-02-09 09:30:11 +0800

    add analytics

---
+++ src/components/Analytics.tsx
@@ -0,0 +1,37 @@
+import Script from 'next/script'
+
+const Analytics = () => (
+    <>
+        <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-TYPJ9DPLLP"
+        />
+        <Script
+            id="google-analytics"
+            dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-TYPJ9DPLLP');
+                `,
+            }}
+        />
+        <Script
+            id="microsoft-clarity"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "kyu46bghqh");
+                `
+            }}
+        />
+    </>
+)
+
+export default Analytics
