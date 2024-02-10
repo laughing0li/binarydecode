65f7f5fe68ef636d64b6e6ec9ce90cc2d8a6e90e
Yunlong Li <yunlong.li@csiro.au>
2024-02-10 11:10:14 +0800

    fix not found logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -245,16 +245,7 @@ function Avatar({
       className={clsx(className, 'pointer-events-auto')}
       {...props}
     >
-      <Image
-        src={avatarImage}
-        alt=""
-        sizes={large ? '4rem' : '2.25rem'}
-        className={clsx(
-          'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
-          large ? 'h-16 w-16' : 'h-9 w-9',
-        )}
-        priority
-      />
+      
     </Link>
   )
 }
