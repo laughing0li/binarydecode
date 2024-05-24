b72b3991ada218f756857dc94e6d3a02e9124882
Yunlong Li <yunlong.li@csiro.au>
2024-05-24 17:43:30 +0800

    add aff program

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
-
+import Image from "next/image";
 import { ContainerInner, ContainerOuter } from '@/components/Container'
+import logo from "@/app/ship.png";
 
 function NavLink({
   href,
@@ -24,8 +25,24 @@ export function Footer() {
     <footer className="mt-32 flex-none">
       <ContainerOuter>
         <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+
           <ContainerInner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+              <Link
+                className="flex items-center gap-2 shrink-0 mt-4 sm:mt-0"
+                href="https://shipfa.st/?via=yunlong"
+                title={`shipfast homepage`}
+              >
+                <span className="text-white">Built with</span>
+                <Image
+                  src={logo}
+                  alt={`shipfast logo`}
+                  className="w-24"
+                  priority={true}
+                  width={100}
+                  height={100}
+                />
+              </Link>
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="https://resumego.io/">ResumeGo</NavLink>
                 {/* <NavLink href="/projects">Projects</NavLink>
