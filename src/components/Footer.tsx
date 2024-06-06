2ae11e40120a157f9da4ff04b510271d567d84de
Yunlong Li <yunlong.li@csiro.au>
2024-06-06 17:34:04 +0800

    add tools to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,62 +1,70 @@
 import Link from 'next/link'
-import Image from "next/image";
+import Image from "next/image"
 import { ContainerInner, ContainerOuter } from '@/components/Container'
-import logo from "@/app/ship.png";
+import logo from "@/app/ship.png"
 
 function NavLink({
-  href,
-  children,
+    href,
+    children,
 }: {
-  href: string
-  children: React.ReactNode
+    href: string
+    children: React.ReactNode
 }) {
-  return (
-    <Link
-      href={href}
-      className="transition hover:text-teal-500 dark:hover:text-teal-400"
-    >
-      {children}
-    </Link>
-  )
+    return (
+        <Link
+            href={href}
+            className="transition hover:text-teal-500 dark:hover:text-teal-400"
+        >
+            {children}
+        </Link>
+    )
 }
 
 export function Footer() {
-  return (
-    <footer className="mt-32 flex-none">
-      <ContainerOuter>
-        <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+    return (
+        <footer className="mt-32 flex-none">
+            <ContainerOuter>
+                <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
 
-          <ContainerInner>
-            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-              <Link
-                className="flex items-center gap-2 shrink-0 mt-4 sm:mt-0"
-                href="https://shipfa.st/?via=yunlong"
-                title={`shipfast homepage`}
-              >
-                <span className="dark:text-white">Built with</span>
-                <Image
-                  src={logo}
-                  alt={`shipfast logo`}
-                  className="w-24 bg-black rounded-xl"
-                  priority={true}
-                  width={100}
-                  height={100}
-                />
-              </Link>
-              <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                <NavLink href="https://resumego.io/">ResumeGo</NavLink>
-                {/* <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/speaking">Speaking</NavLink>
-                <NavLink href="/uses">Uses</NavLink> */}
-              </div>
-              <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Binary Decode. All rights
-                reserved.
-              </p>
-            </div>
-          </ContainerInner>
-        </div>
-      </ContainerOuter>
-    </footer>
-  )
+                    <ContainerInner>
+                        <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+                            <Link
+                                className="flex items-center gap-2 shrink-0 mt-4 sm:mt-0"
+                                href="https://shipfa.st/?via=yunlong"
+                                title={`shipfast homepage`}
+                            >
+                                <span className="dark:text-white">Built with</span>
+                                <Image
+                                    src={logo}
+                                    alt={`shipfast logo`}
+                                    className="w-24 bg-black rounded-xl"
+                                    priority={true}
+                                    width={100}
+                                    height={100}
+                                />
+                            </Link>
+                            <div className="grid grid-cols-1 gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+                                <Link href="https://resumego.io/" target="blank" title="AI Resume tailor" className="link link-hover">
+                                    ResumeGo
+                                </Link>
+                                <Link href="https://photorater.io/" target="blank" title="AI photo rating app" className="link link-hover">
+                                    Photo Rater
+                                </Link>
+                                <Link href="https://sunodownloader.io/" target="blank" title="Suno music downloader" className="link link-hover">
+                                    Suno Downloader
+                                </Link>
+                                <Link href="https://soundifytext.io/" target="blank" title="AI sound effects generator" className="link link-hover">
+                                    Soundify
+                                </Link>
+                            </div>
+                            <p className="text-sm text-zinc-400 dark:text-zinc-500">
+                                &copy; {new Date().getFullYear()} Binary Decode. All rights
+                                reserved.
+                            </p>
+                        </div>
+                    </ContainerInner>
+                </div>
+            </ContainerOuter>
+        </footer>
+    )
 }
